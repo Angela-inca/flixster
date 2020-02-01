777fed3f9001ffa6cd10f7ef6d4b8b1d7e1a0991
Angela <y1gu@ucsd.edu>
2020-02-01 00:54:32 -0800

    add the collectionView and the tab bar

---
+++ flixster/MoviegridCell.swift
@@ -0,0 +1,18 @@
+//
+//  MoviegridCell.swift
+//  flixster
+//
+//  Created by angela on 1/30/20.
+//  Copyright © 2020 angela. All rights reserved.
+//
+
+import UIKit
+
+class MoviegridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+    
+    
+}
